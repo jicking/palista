712cd10a9a1bede6754a238634d7d137ea4df5b1
jicking <7297029+jicking@users.noreply.github.com>
2024-09-15 00:32:49 +0800

    set app as spa

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<nav>
+	<u>
+		<li><a href="/">Home</a></li>
+		<li><a href="/about">About</a></li>
+		<li><a href="/account/login">Account</a></li>
+	</u>
+</nav>
+
+<main>
+	<slot></slot>
+</main>
